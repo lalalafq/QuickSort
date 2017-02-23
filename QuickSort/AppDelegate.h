ac64198e1b500f6ac7affa9d0e975d9f44e23d2e
fuqi <wb-fq258946@alibaba-inc.com>
2017-02-23 14:24:14 +0800

    Initial Commit

---
+++ QuickSort/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuickSort
+//
+//  Created by lifeng on 17/2/23.
+//  Copyright © 2017年 fq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuickSort/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickSort
+//
+//  Created by lifeng on 17/2/23.
+//  Copyright © 2017年 fq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
